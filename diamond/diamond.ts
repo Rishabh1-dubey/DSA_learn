f55639e2985a794791e9bf25e545863df1ba9c0d
Rishabh1-dubey <rishabhdubey006@gmail.com>
2024-02-01 09:46:08 +0530

    added more

---
+++ diamond/diamond.ts
@@ -0,0 +1,28 @@
+let n=7;
+function diamondPrinter(n:number){
+    let stars=1;
+    let spaces= Math.floor(n/2)
+    let s:string= "";
+
+    for(let i=1; i<=n; i++){
+         
+        for(let j= 1; j<=spaces; j++){
+            s+=" ";
+        }
+        for (let j=1; j<=stars; j++){
+            s+="*";
+        }
+
+        s+="\n";
+
+        if(i<=Math.floor(n/2)){
+            stars+=2;
+            spaces-=1;
+        }else{
+            stars-=2;
+            spaces+=1;
+        }
+    }
+    console.log(s)
+}
+diamondPrinter(n);
